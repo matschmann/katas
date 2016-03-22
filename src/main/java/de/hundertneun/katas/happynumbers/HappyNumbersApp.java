dc0e30aa4139b1e64f928d086d5e632674b30a50
Rainer Standke <rainer.standke@saascom.de>
2016-03-22 09:12:56 +0100

    created method for splitting number in digits

--- src/main/java/de/hundertneun/katas/happynumbers/HappyNumbersApp.java
+++ src/main/java/de/hundertneun/katas/happynumbers/HappyNumbersApp.java
@@ -7,8 +7,22 @@ public class HappyNumbersApp {
     }
 
     public boolean isNumberHappy(int number) {
+        int[] digits = getDigits(number);
         
+        
+
         return false; 
+    }
+
+    private int[] getDigits(int number) {
+        String numberAsString = Integer.toString(number);
+
+        int[] digits = new int[numberAsString.length()];
+        for (int i = 0; i<numberAsString.length();i++) {
+            digits[i] = numberAsString.charAt(i);
+        }
+        
+        return digits; 
     }
 
 }
